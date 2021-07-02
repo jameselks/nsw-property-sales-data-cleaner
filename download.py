bb2263d86dbe2d4ea9e502862f7902fa6531b142
James <republicofelk@gmail.com>
2021-07-03 09:46:20 +1000

    Download data files

---
+++ download.py
@@ -0,0 +1,31 @@
+# 
+
+print('Hello! Python is up and running.')
+import time
+start = time.time()
+
+import urllib.request
+from datetime import datetime, date, timedelta
+
+url_base = 'https://www.valuergeneral.nsw.gov.au/__psi/'
+url_base_weekly = 'weekly/'
+url_base_yearly = 'yearly/'
+now = datetime.now()
+this_year = now.year
+years_to_collect = 6
+
+d = datetime(this_year, 1, 7)
+offset = -d.weekday() #weekday = 0 means monday
+this_date = (d + timedelta(offset))
+
+while this_date < (now - timedelta(days=7)):
+    this_date = this_date + timedelta(days=7)  
+    download_url = url_base + url_base_weekly + this_date.strftime('%Y%m%d') + '.zip'
+    print('Downloading... ' + download_url)
+    urllib.request.urlretrieve(download_url, 'data/' + this_date.strftime('%Y%m%d') + '.zip')
+
+for year in range(int(this_year-years_to_collect), int(this_year)):
+    download_url = url_base + url_base_yearly + str(year) + '.zip'
+    print('Downloading... ' + download_url)
+    urllib.request.urlretrieve(download_url, 'data/' + str(year) + '.zip')
+
