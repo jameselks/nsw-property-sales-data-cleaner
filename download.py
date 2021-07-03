a0272df4211e9ca742e2787e024783d0d9bc58e8
James <republicofelk@gmail.com>
2021-07-03 10:38:29 +1000

    Create download dir if it doesn't exist

--- download.py
+++ download.py
@@ -4,15 +4,20 @@ print('Hello! Python is up and running.')
 import time
 start = time.time()
 
+import os
 import urllib.request
 from datetime import datetime, date, timedelta
 
+download_dir = 'data/'
 url_base = 'https://www.valuergeneral.nsw.gov.au/__psi/'
 url_base_weekly = 'weekly/'
 url_base_yearly = 'yearly/'
 now = datetime.now()
 this_year = now.year
 years_to_collect = 6
+
+if not os.path.isdir(download_dir):
+    os.mkdir(download_dir)
 
 d = datetime(this_year, 1, 7)
 offset = -d.weekday() #weekday = 0 means monday
@@ -22,10 +27,10 @@ while this_date < (now - timedelta(days=7)):
     this_date = this_date + timedelta(days=7)  
     download_url = url_base + url_base_weekly + this_date.strftime('%Y%m%d') + '.zip'
     print('Downloading... ' + download_url)
-    urllib.request.urlretrieve(download_url, 'data/' + this_date.strftime('%Y%m%d') + '.zip')
+    urllib.request.urlretrieve(download_url, download_dir + this_date.strftime('%Y%m%d') + '.zip')
 
 for year in range(int(this_year-years_to_collect), int(this_year)):
     download_url = url_base + url_base_yearly + str(year) + '.zip'
     print('Downloading... ' + download_url)
-    urllib.request.urlretrieve(download_url, 'data/' + str(year) + '.zip')
+    urllib.request.urlretrieve(download_url, download_dir + str(year) + '.zip')
 
