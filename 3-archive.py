df9c5a4a0ac2c406a5d43c7028e67683e6407c23
James <republicofelk@gmail.com>
2025-03-15 15:43:59 +1100

    Add logging to zip archive creation process for better traceability

--- 3-archive.py
+++ 3-archive.py
@@ -1,12 +1,14 @@
-# 
-
-print('Creating zip archive')
+import logging
 import time
-start = time.time()
-
 import os
 import zipfile
 from datetime import datetime
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info('Creating zip archive')
+start = time.time()
 
 name_original = 'extract-3-very-clean.csv'
 name_new = 'nsw-property-sales-data-updated' + datetime.now().strftime('%Y%m%d')
@@ -20,5 +22,5 @@ os.rename(name_new + '.csv', name_original)
 # Also create a generic a copy that doesn't have the date attached
 os.system('cp ' + name_new + '.zip' + ' ' + 'archive.zip') 
 
-print("Complete: zip archive has been created.")
-print('Total elapsed time was ' + str(int(time.time() - start)) + " seconds")
+logging.info("Complete: zip archive has been created.")
+logging.info('Total elapsed time was ' + str(int(time.time() - start)) + " seconds")
